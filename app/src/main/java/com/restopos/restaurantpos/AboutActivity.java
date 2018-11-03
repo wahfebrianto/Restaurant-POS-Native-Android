c067e69b46677a85a74272dac219ce05fe5e3883
Wahyu <wahfebrianto@gmail.com>
2018-11-03 23:44:21 +0700

    Main navigation and about page

---
+++ app/src/main/java/com/restopos/restaurantpos/AboutActivity.java
@@ -0,0 +1,32 @@
+package com.restopos.restaurantpos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        TextView tWebsite = (TextView) findViewById(R.id.about_website);
+        tWebsite.setMovementMethod(LinkMovementMethod.getInstance());
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id==android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
